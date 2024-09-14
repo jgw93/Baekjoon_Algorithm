46192d6cfa66b8ad0fef82fedf27ca1e86d6251f
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-14 20:28:56 +0900

    [Bronze V] Title: 구구단, Time: 188 ms, Memory: 18524 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int A = sc.nextInt();
+        int result;
         
-        for(int i = 1; i <= 9; i++) {
-            System.out.println(A + " * " + i + " = " + (A * i));
+        for (int i = 1; i <= 9; i++) {
+            result = A * i;
+            System.out.println(A + " * " + i + " = " + result);
         }
     }
 }
