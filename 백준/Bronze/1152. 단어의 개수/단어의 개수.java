2b85257bbca6859b4f3149a52c6ab32d70d16037
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-20 20:04:57 +0900

    [Bronze II] Title: 단어의 개수, Time: 608 ms, Memory: 64420 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String S = sc.nextLine();
+       
+        String[] words = S.trim().split("\\s+");
+        
+        if(S.trim().isEmpty()){
+            System.out.println(0);
+        }else{
+            System.out.println(words.length);
+        }
+    }
+}
