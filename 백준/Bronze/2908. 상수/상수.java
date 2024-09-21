13a98ae2cbf2363dcdbd8bac90b355c9f29bbf19
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-21 18:18:54 +0900

    [Bronze II] Title: 상수, Time: 100 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,22 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
     
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        String A = sc.next();
-        String B = sc.next();
+        String[] input = br.readLine().split(" ");
         
-        int reverseA = reverseNumber(A);
-        int reverseB = reverseNumber(B);
+        int reverseA = reverseNumber(Integer.parseInt(input[0]));
+        int reverseB = reverseNumber(Integer.parseInt(input[1]));
         
         System.out.println(Math.max(reverseA, reverseB));
     }
     
-    public static int reverseNumber(String number){
-        StringBuilder sb = new StringBuilder(number);
-        sb.reverse();
-        return Integer.parseInt(sb.toString());
+    public static int reverseNumber(int number) {
+        int reverse = 0;
+        while (number > 0) {
+            reverse = reverse * 10 + (number % 10);
+            number /= 10;
+        }
+        return reverse;
     }
 }
