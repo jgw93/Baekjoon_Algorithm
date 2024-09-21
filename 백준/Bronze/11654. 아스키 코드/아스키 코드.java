11ff7ac7b36a4ccafe60f9b8169215d77604f400
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-21 18:17:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
     
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        String S = sc.next();
-        
-        System.out.println((int) S.charAt(0));
+        System.out.println((int) br.readLine().charAt(0));
     }
 }
