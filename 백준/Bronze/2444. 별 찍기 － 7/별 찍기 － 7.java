2faf2bafb1909572bf9e02a6f9015be89265296b
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-23 20:39:30 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 164 ms, Memory: 17844 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,34 +1,24 @@
 import java.util.Scanner;
 
-public class Main{
-    
-    public static void main(String[] args){
+public class Main {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < N; i++){
-            
-            for(int j = 0; j < N - i - 1; j++){
-                System.out.print(" ");
-            }
-            
-            for(int j = 0; j < 2 * i + 1; j++){
-                System.out.print("*");
-            }
-          System.out.println();
+        for (int i = 0; i < N; i++) {
+            sb.append(" ".repeat(N - i - 1));
+            sb.append("*".repeat(2 * i + 1));
+            sb.append("\n");
         }
-        
-        for(int i = N - 2; i >= 0; i--){
-            
-            for(int j = 0; j < N - i - 1; j++){
-                System.out.print(" ");
-            }
-            
-            for(int j = 0; j < 2 * i + 1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+
+        for (int i = N - 2; i >= 0; i--) {
+            sb.append(" ".repeat(N - i - 1));
+            sb.append("*".repeat(2 * i + 1));
+            sb.append("\n");
         }
+        System.out.print(sb);
     }
 }
