8b02642750811b09ed40d63046576fd57e406562
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-24 19:28:18 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 100 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,25 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
-public class Main{
-    
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        String S = sc.next();
-        
-        if(isPalindrome(S)){
-            System.out.println(1);
-        }else{
-            System.out.println(0);
+        String S = br.readLine();
+
+        if (isPalindrome(S)) {
+            bw.write("1\n");
+        } else {
+            bw.write("0\n");
         }
+        
+        bw.flush();
+        bw.close();
+        br.close();
     }
     
-    public static boolean isPalindrome(String S){
+    public static boolean isPalindrome(String S) {
         int length = S.length();
         
-        for(int i = 0; i < length / 2; i++){
-            
-            if(S.charAt(i) != S.charAt(length - i - 1)){
+        for (int i = 0; i < length / 2; i++) {
+            if (S.charAt(i) != S.charAt(length - i - 1)) {
                 return false;
             }
         }
