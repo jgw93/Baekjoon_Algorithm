038f6b2446f16b172139ecc987582bcaa5b5e4c7
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-24 19:26:29 +0900

    [Bronze I] Title: 단어 공부, Time: 260 ms, Memory: 21268 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,29 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
-public class Main{
-    
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        String S = sc.next().toUpperCase();
-        int[] A = new int[26];
-        
-        for(int i = 0; i < S.length(); i++){
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine().toUpperCase();
+        int[] frequency = new int[26];
+
+        int maxCount = 0;
+        char mostFrequent = '?';
+
+        for (int i = 0; i < S.length(); i++) {
             char c = S.charAt(i);
-            A[c - 'A']++;
-        }
-        
-        int max = 0;
-        char most = '?';
-        
-        for(int i = 0; i < 26; i++){
-            if(A[i] > max){
-                max = A[i];
-                most = (char)(i + 'A');
-            }else if(A[i] == max){
-                most = '?';
+            int index = c - 'A';
+            frequency[index]++;
+
+            if (frequency[index] > maxCount) {
+                maxCount = frequency[index];
+                mostFrequent = c;
+            } else if (frequency[index] == maxCount) {
+                mostFrequent = '?';
             }
         }
-        System.out.println(most);
+        bw.write(mostFrequent);
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
