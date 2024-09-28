f8e28218dda8067ecd7f8456ad356cbff4956223
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-28 20:03:39 +0900

    [Bronze II] Title: 진법 변환, Time: 112 ms, Memory: 15920 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,15 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
-public class Main{
+public class Main {
     
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        String N = sc.next();
-        int B = sc.nextInt();
+        String[] input = br.readLine().split(" ");
+        String N = input[0];
+        int B = Integer.parseInt(input[1]);
         
         int value = Integer.parseInt(N, B);
         
-        System.out.println(value);
+        bw.write(value + "\n");
+        
+        bw.flush();
+        
+        br.close();
+        bw.close();
     }
 }
