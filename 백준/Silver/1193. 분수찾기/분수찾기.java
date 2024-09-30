96e30b6444399b66517709400f6a299cd180baf2
장규원 <159901575+jgw93@users.noreply.github.com>
2024-09-30 18:17:04 +0900

    [Silver V] Title: 분수찾기, Time: 184 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,32 +1,28 @@
 import java.util.Scanner;
 
-public class Main{
-    
-    public static void main(String[] args){
+public class Main {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int X = sc.nextInt();
-        int D = 1;
-        int sum = 1;
         
-        while(X > sum){
-            D++;
-            sum += D;
-        }
-        
+        int D = (int)(Math.ceil((-1 + Math.sqrt(1 + 8 * X)) / 2));
+        int sum = D * (D + 1) / 2;
+
         int indexInD = X - (sum - D);
         int numerator, denominator;
-        
-         if (D % 2 == 0) {
+
+        if (D % 2 == 0) {
             numerator = indexInD;
             denominator = D - indexInD + 1;
         } else {
             numerator = D - indexInD + 1;
             denominator = indexInD;
         }
-      
+
         System.out.println(numerator + "/" + denominator);
-        
+
         sc.close();
     }
 }
