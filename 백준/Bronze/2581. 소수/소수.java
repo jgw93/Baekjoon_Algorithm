f29350d96256cdb8c96259fab53023a55f1c66c0
장규원 <159901575+jgw93@users.noreply.github.com>
2024-10-03 06:01:16 +0900

    [Bronze II] Title: 소수, Time: 160 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = M; i <= N; i++){
+            if(isPrime(i)){
+                sum += i;
+                if(i < min){
+                    min = i;
+                }
+            }
+        }
+        
+        if(sum == 0){
+            System.out.println(-1);
+        }else{
+            System.out.println(sum);
+            System.out.println(min);
+        }
+        sc.close();
+    }
+}
