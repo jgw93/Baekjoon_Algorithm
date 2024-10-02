17bc9ad6ab7fb98edee6a507bbc8365968f71beb
장규원 <159901575+jgw93@users.noreply.github.com>
2024-10-03 06:06:59 +0900

    [Bronze II] Title: 소수, Time: 116 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,40 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
-public class Main{
-    
+public class Main {
+
     public static boolean isPrime(int num) {
-        if (num < 2) return false;
-        for (int i = 2; i * i <= num; i++) {
+        if (num == 2) return true;
+        if (num < 2 || num % 2 == 0) return false;
+        for (int i = 3; i * i <= num; i += 2) {
             if (num % i == 0) {
                 return false;
             }
         }
         return true;
     }
-    
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int M = sc.nextInt();
-        int N = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
         int sum = 0;
         int min = Integer.MAX_VALUE;
-        
-        for(int i = M; i <= N; i++){
-            if(isPrime(i)){
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
                 sum += i;
-                if(i < min){
+                if (i < min) {
                     min = i;
                 }
             }
         }
-        
-        if(sum == 0){
-            System.out.println(-1);
-        }else{
-            System.out.println(sum);
-            System.out.println(min);
+
+        if (sum == 0) {
+            bw.write("-1\n");
+        } else {
+            bw.write(sum + "\n");
+            bw.write(min + "\n");
         }
-        sc.close();
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
