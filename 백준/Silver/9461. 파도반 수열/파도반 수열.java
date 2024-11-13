7a9ef5b40b03ba3ce42f97ea75ce203d16b2d219
장규원 <159901575+jgw93@users.noreply.github.com>
2024-11-13 18:00:28 +0900

    [Silver III] Title: 파도반 수열, Time: 188 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+        long[] dp = new long[101];
+        
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        
+        for(int i = 4; i <= 100; i++){
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+        
+        while(T-- > 0){
+            int N = sc.nextInt();
+            System.out.println(dp[N]);
+        }
+        sc.close();
+    }
+}
