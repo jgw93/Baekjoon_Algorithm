9e401cdcb49a37b1f1695b8f2d3eb7d5f24a75e5
장규원 <159901575+jgw93@users.noreply.github.com>
2024-11-14 16:10:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 784 ms, Memory: 132672 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[][] triangle = new int[n][n];
+        int[][] dp = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = sc.nextInt();
+            }
+        }
+        
+        dp[0][0] = triangle[0][0];
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                } else if (j == i) {
+                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+                }
+            }
+        }
+        
+        int maxSum = 0;
+        for (int j = 0; j < n; j++) {
+            maxSum = Math.max(maxSum, dp[n - 1][j]);
+        }
+        
+        System.out.println(maxSum);
+        sc.close();
+    }
+}
